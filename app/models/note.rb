17888ef8c52a0da7b34c8a640245ded9946049e4
Rahul Gupta <rahul11061995@gmail.com>
2015-06-18 23:38:17 +0530

    Add Basic Validations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,7 @@
 class Note < ActiveRecord::Base
 	belongs_to :user
-	has_many :shared_users
-	has_many :tags
+	has_many :shared_users,:dependent=>:destroy
+	has_many :tags,:dependent=>:destroy
+
+	validates :content,presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
 	#Encrypt password
 	has_secure_password
 
+	#Validation
+	validates :username,:email,:password,presence: true
+	validates :username,:email, uniqueness: {:case_sensitive=>false}
+	
 end
