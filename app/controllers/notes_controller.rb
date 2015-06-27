fe8785475b43e01c374e8b37e25ea4514369cde8
Rahul Gupta <rahul11061995@gmail.com>
2015-06-28 02:06:23 +0530

    Extract, update & display tags from note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+ 
   def index
     @notes = Note.recent_notes
     @note = Note.new
@@ -9,8 +10,15 @@ class NotesController < ApplicationController
   end
 
   def create
+    
     note = Note.new(get_user_params)
     if note.save
+      # Extract tags
+      tags = get_tags
+      unless tags.empty?
+        #save tags
+        save_tags(tags,note)
+      end
       flash[:success]="Note added."
       redirect_to(notes_path)
     else
@@ -28,6 +36,17 @@ class NotesController < ApplicationController
   def update
     note = Note.find_by_id(params[:id])
     if note.update_attributes(get_user_params)
+      # Extract tags
+      tags = get_tags
+      unless tags.empty?
+        #remove previous tags of this note
+        note.tags.each do |tag|
+          tag.destroy
+          puts " >>> Tag #{tag.tagname} Destroyed .. "
+        end
+        #save tags
+        save_tags(tags,note)
+      end
       flash[:success]="Note updated."
       redirect_to(notes_path)
     else
@@ -48,5 +67,25 @@ class NotesController < ApplicationController
 
   def get_user_params
     params.require(:note).permit(:content)
+  end
+
+  def get_tags
+    content = params[:note][:content]
+    tags_with_junk = content.split("#")
+    tags_with_junk.shift
+    #remove spaces 
+    tags = []
+    tags_with_junk.each do |junk_tags| 
+      tags << junk_tags.split(" ")[0]
+    end
+    return tags
+  end
+
+  def save_tags(tags,note)
+    tags.each do |tag|
+      new_tag = Tag.new(:tagname=>tag)
+      note.tags << new_tag
+      puts ">>> Tags added are : #{tag}"
+    end
   end
 end
