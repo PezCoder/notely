bc29c787bb36dffc82429e83f5d9d24bde551821
PezCoder <rahul11061995@gmail.com>
2015-08-22 14:47:16 +0530

    Remove traces of shared_users

--- db/migrate/20150618150224_create_notes.rb
+++ db/migrate/20150618150224_create_notes.rb
@@ -2,7 +2,6 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
     	t.text 'content'
-    	t.references :user
       t.timestamps null: false
     end
     add_index('notes','user_id')
