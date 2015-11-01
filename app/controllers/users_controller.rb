4e2791bd18fd3a04fa41800a42f3dd097b5e91ef
PezCoder <rahul11061995@gmail.com>
2015-11-01 05:47:43 +0530

    Some layout modification & User Guide on registration & landing page provided

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,14 @@ class UsersController < ApplicationController
 	end
 
 	def create 
+		#registering a new user
 		@user = User.new(get_user_params)
 		if @user.save
 			flash[:notice]="Your account is created succesfully."
+			#add a note to give user's instructions of what to do 
+			note = Note.create(:content=>"Here's a #tip !\nTo tag a note use #hash symbol & to share/collab. your notes use @friendname symbol. \nSee ! It's that easy.")
+			Collaboration.create(:user=>@user,:note=>note,:is_admin=>true)
+
 			redirect_to user_notes_path(@user.id)
 		else
 			flash[:alert]="Error while creating account ! Please try again later. "
